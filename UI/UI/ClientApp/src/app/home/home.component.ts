2fac8ce5c2a0895cd23e0e05d85fa5cc767cf2de
davslye <davslye@gmail.com>
2021-05-08 05:54:32 -0500

    1st deploy to azure

--- UI/UI/ClientApp/src/app/home/home.component.ts
+++ UI/UI/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { parse } from 'querystring';
+import { Component } from '@angular/core';
 import { FizzBuzzComponent } from '../fizzBuzz/fizzBuzz.component';
 
 @Component({
